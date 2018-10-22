b66627ab6b01b09e540e242b5d6cd08ec529c677
kitamuyu <33745987+kitamuyu@users.noreply.github.com>
2018-10-22 21:42:09 +0900

    text changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => (
   <Layout>
     <h1>learn</h1>
     <p>Welcome to kitamura react site</p>
-    <p>Now go build something great.</p>
+    <p>let's go.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
