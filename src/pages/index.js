cfbc29a55e4ff6fc536bffa041a84177c8132988
kitamuyu <33745987+kitamuyu@users.noreply.github.com>
2018-10-25 00:22:20 +0900

    update CSS animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,8 @@ const IndexPage = () => (
   <Layout>
     <div className="Hero">
       <div className="HeroGroup">
-        <h1>That's the closest WAY to your new bro</h1>
-        <p>Welcome to satudora digital SNS page, WAY.</p>
-        <p>Who Are You? What's your skill? 
+        <h1>That's the<br/>closest WAY to<br/>your new bro</h1>
+        <p>Welcome to satudora digital SNS page,WAY. Who Are You? What's your skill? 
           To begin with, why don't you tell me about yourself?</p>
         <Link to="/page-2/">Register</Link>
       </div>
