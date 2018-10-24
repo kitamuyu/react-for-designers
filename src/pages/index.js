a3e239015906626076c002995e0dbb93d5c6c823
kitamuyu <33745987+kitamuyu@users.noreply.github.com>
2018-10-24 20:32:14 +0900

    Update layouts

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,11 @@ const IndexPage = () => (
   <Layout>
     <div className="Hero">
       <div className="HeroGroup">
-        <h1>learn</h1>
-        <p>Welcome to kitamura react site</p>
-        <p>let's go</p>
-        <Link to="/page-2/">Go to page 2</Link>
+        <h1>That's the closest WAY to your new bro</h1>
+        <p>Welcome to satudora digital SNS page, WAY.</p>
+        <p>Who Are You? What's your skill? 
+          To begin with, why don't you tell me about yourself?</p>
+        <Link to="/page-2/">Register</Link>
       </div>
     </div>
   </Layout>
